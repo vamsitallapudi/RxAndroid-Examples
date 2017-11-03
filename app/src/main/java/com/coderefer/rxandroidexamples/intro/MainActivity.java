1858b0635b0cb2d63f3cf2b8bf8d7a98e00b6708
vamsitallapudi <mail4vamsikrishna@gmail.com>
2017-11-04 00:45:30 +0530

    Initial Commit

---
+++ app/src/main/java/com/coderefer/rxandroidexamples/intro/MainActivity.java
@@ -0,0 +1,68 @@
+package com.coderefer.rxandroidexamples.intro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+
+import com.coderefer.rxandroidexamples.R;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Observable<Integer> mObservable;
+    private Observer<Integer> mObserver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initializeObservable();
+        initializeObserver();
+        mObservable.subscribe(mObserver);
+    }
+
+    private void initializeObservable() {
+        mObservable = Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+
+                for(int i=1; i<=5;i++){
+                    e.onNext(i);
+                }
+                e.onComplete();
+            }
+        });
+    }
+
+    private void initializeObserver() {
+        mObserver = new Observer<Integer>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Toast.makeText(MainActivity.this, "onSubscribe called", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                Toast.makeText(MainActivity.this, "onNext called: " + integer, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Toast.makeText(MainActivity.this, "onError called", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onComplete() {
+                Toast.makeText(MainActivity.this, "onComplete called", Toast.LENGTH_SHORT).show();
+            }
+        };
+    }
+
+}
