e1a0b1d8773a55ea8284407a828d8fe392f6480f
Vamsi Tallapudi <vamsi@coderefer.com>
2018-09-17 23:10:05 +0530

    adding travis ci and adding few unit test cases

---
+++ app/src/test/java/com/coderefer/rxandroidexamples/ExampleUnitTest.kt
@@ -0,0 +1,36 @@
+package com.coderefer.rxandroidexamples
+
+import io.reactivex.Observable
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+class ExampleUnitTest {
+    @Test
+    @Throws(Exception::class)
+    fun addition_isCorrect() {
+        assertEquals(4, (2 + 2).toLong())
+    }
+
+    @Test
+    fun testCreate_withSubscriber() {
+        val observable = Observable.create<Int> {
+            subscriber->
+            run {
+                System.out.println("starting")
+                subscriber.onNext(5)
+                subscriber.onNext(6)
+                subscriber.onNext(7)
+                subscriber.onComplete()
+                System.out.println("completed")
+
+            }
+        }
+        observable.subscribe { System.out.println("next: $it") }
+    }
+}
